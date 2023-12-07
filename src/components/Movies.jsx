7b8be1b5b21e9d94559b3723de4bd41a1b11ca40
HighMid <xstar671@gmail.com>
2023-12-06 20:59:44 -0500

    Added movie component for movie API

---
+++ src/components/Movies.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+const Movies = ({ movies }) => {
+    if (!movies || movies.length === 0) {
+        return <div>No movie data available.</div>;
+    }
+
+    return (
+        <div>
+            {movies.map((movie, index) => (
+                <Card key={index}>
+                    <Card.Img variant="top" src={movie.image_url} />
+                    <Card.Body>
+                        <Card.Title>{movie.title}</Card.Title>
+                        <Card.Text>{movie.overview}</Card.Text>
+                        {/* Other movie details */}
+                    </Card.Body>
+                </Card>
+            ))}
+        </div>
+    );
+};
+
+export default Movies;
